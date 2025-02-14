eecccf7bec9b76f2b6ab11adec4932f269f5c62c
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-02-13 23:02:48 -0300

    feat: add barrel exports for utility functions

---
+++ src/lib/index.ts
@@ -0,0 +1,11 @@
+export { cn } from "./tailwindMerge";
+export {
+  stylistColors,
+  roundToNearestFiveMinutes,
+  getInitialTimeSlot,
+  checkAppointmentOverlap,
+  getAppointmentStyle,
+  MIN_APPOINTMENT_DURATION,
+  DRAG_SNAP_DURATION,
+  getValidResizeTime,
+} from "./calendar";
