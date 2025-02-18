c4aa6977466864d2dd5e660647a3134202bb2b9f
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-02-18 01:15:45 -0300

    fix: update page title

--- src/views/agenda/index.tsx
+++ src/views/agenda/index.tsx
@@ -5,7 +5,7 @@ import CalendarWrapper from "@/components/calendar/CalendarWrapper";
 export const AgendaView = (): JSX.Element => {
   return (
     <>
-      <h1 className="text-2xl font-bold mb-4">Calendario</h1>
+      <h1 className="text-2xl font-bold mb-4">Agenda</h1>
 
       <CalendarWrapper />
     </>
