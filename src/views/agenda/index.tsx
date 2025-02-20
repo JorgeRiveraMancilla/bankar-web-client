f778577071d40b12705374ce305523dd06b94e28
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-02-20 01:45:53 -0300

    feat: add stylists legend and management

--- src/views/agenda/index.tsx
+++ src/views/agenda/index.tsx
@@ -1,13 +1,24 @@
+"use client";
+
 import { JSX } from "react";
 
+import { useStylists } from "@/hooks/useStylists";
+
 import { CalendarWrapper } from "./calendar-wrapper/CalendarWrapper";
+import { Legends } from "./legends/Legends";
 
 export const AgendaView = (): JSX.Element => {
+  const { stylists, handleStylistUpdate } = useStylists();
+
   return (
     <>
-      <h1 className="text-2xl font-bold mb-4">Agenda</h1>
+      <div className="flex items-center gap-4 mb-4">
+        <h1 className="text-2xl font-bold">Agenda</h1>
 
-      <CalendarWrapper />
+        <Legends stylists={stylists} onStylistUpdate={handleStylistUpdate} />
+      </div>
+
+      <CalendarWrapper stylists={stylists} />
     </>
   );
 };
